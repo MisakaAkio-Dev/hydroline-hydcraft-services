fb9eef18906ffcfde41ba4ff963e43fd87aa4ce0
AurLemon <aurlemoncn@gmail.com>
2025-12-05 04:20:39 +0800

    chore(player): 调整文本描述

--- frontend/src/views/admin/components/PlayerDetailDialog.vue
+++ frontend/src/views/admin/components/PlayerDetailDialog.vue
@@ -935,7 +935,7 @@ watch(selectedServerId, (serverId, previous) => {
                   class="rounded-xl border border-slate-200 dark:border-slate-800 p-3 bg-white/80 dark:bg-slate-900/40"
                 >
                   <p class="text-xs text-slate-500 dark:text-slate-500">
-                    用了几次斧头
+                    用了几次建筑手杖
                   </p>
                   <p
                     class="text-xl font-semibold text-slate-900 dark:text-white"
